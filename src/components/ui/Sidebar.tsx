18178659b6f8407c9ce9f64487faed9b1be22ee0
creativerze <creativerze@gmail.com>
2023-10-14 20:12:19 +0600

    added userRole, adminRole and superAdmin role in sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -2,49 +2,15 @@
 
 import { Layout, Menu } from "antd";
 import { useState } from "react";
-import type { MenuProps } from "antd";
 const { Sider } = Layout;
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
 
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
+import { sidebarItems } from "@/constants/sidebarItems";
+import { USER_ROLE } from "@/constants/role";
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+
+  const role = USER_ROLE.USER;
 
   return (
     <Sider
@@ -77,7 +43,7 @@ const Sidebar = () => {
         theme="dark"
         defaultSelectedKeys={["1"]}
         mode="inline"
-        items={items}
+        items={sidebarItems(role)}
       />
     </Sider>
   );
--- src/constants/sidebarItems.tsx
+++ src/constants/sidebarItems.tsx
@@ -1,3 +1,66 @@
+import type { MenuProps } from "antd";
+import { TableOutlined, ProfileOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { USER_ROLE } from "./role";
+
 export const sidebarItems = (role: string) => {
-  return;
+  const defaultSidebarItems: MenuProps["items"] = [
+    {
+      label: "profile",
+      key: "profile",
+      icon: <ProfileOutlined />,
+      children: [
+        {
+          label: <Link href={`${role}/ profile`}>Account Profile</Link>,
+          key: `/${role}/profile`,
+        },
+        {
+          label: <Link href={`${role}/ edit-profile`}>Edit Profile</Link>,
+          key: `/${role}/edit-profile`,
+        },
+      ],
+    },
+  ];
+
+  const userSidebarItems: MenuProps["items"] = [
+    ...defaultSidebarItems,
+    {
+      label: <Link href={`${role}/manage-booking`}>Manage Booking</Link>,
+      key: `${role}/manage-booking`,
+      icon: <TableOutlined />,
+    },
+  ];
+
+  const commonAdminSidebarItems: MenuProps["items"] = [
+    {
+      label: <Link href={`${role}/manage-user`}>Manage User</Link>,
+      key: `${role}/manage-user`,
+      icon: <TableOutlined />,
+    },
+  ];
+
+  const adminSidebarItems: MenuProps["items"] = [
+    ...defaultSidebarItems,
+    ...commonAdminSidebarItems,
+    // {
+    //   label: <Link href={`${role}/manage-user`}>Manage User</Link>,
+    //   key: `${role}/manage-user`,
+    //   icon: <TableOutlined />,
+    // },
+  ];
+
+  const superAdminSidebarItems: MenuProps["items"] = [
+    ...defaultSidebarItems,
+    ...commonAdminSidebarItems,
+    {
+      label: <Link href={`${role}/manage-admin`}>Manage Admin</Link>,
+      key: `${role}/manage-admin`,
+      icon: <TableOutlined />,
+    },
+  ];
+
+  if (role === USER_ROLE.SUPER_ADMIN) return superAdminSidebarItems;
+  else if (role === USER_ROLE.ADMIN) return adminSidebarItems;
+  else if (role === "super_admin") return userSidebarItems;
+  else return defaultSidebarItems;
 };
