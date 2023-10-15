f9298e9311b8616b1a5483b2a34a0e10098e0c7e
creativerze <creativerze@gmail.com>
2023-10-16 01:17:08 +0600

    added edit-user

--- src/app/(withlayout)/admin/page.tsx
+++ src/app/(withlayout)/admin/page.tsx
@@ -1,17 +1,35 @@
 "use client";
 
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
 import { useGetSingleUserQuery } from "@/redux/features/user/userApi";
 import { getUserInfo } from "@/services/auth.service";
 import React from "react";
 
 const AdminPage = () => {
-  const { _id } = getUserInfo() as any;
+  const { _id, role } = getUserInfo() as any;
 
   const { data, isLoading } = useGetSingleUserQuery(_id);
 
   const userInfo = data?.data;
   return (
     <div>
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+        ]}
+      />
+      <h1
+        style={{
+          marginBottom: "20px",
+          color: "green",
+        }}
+      >
+        Admin Information
+      </h1>
+
       <h1>Name: {userInfo?.name}</h1>
       <h1>Email: {userInfo?.email}</h1>
       <h1>ContactNo: {userInfo?.contactNo}</h1>
--- src/app/(withlayout)/user/page.tsx
+++ src/app/(withlayout)/user/page.tsx
@@ -1,9 +1,39 @@
+"use client";
+
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { useGetSingleUserQuery } from "@/redux/features/user/userApi";
+import { getUserInfo } from "@/services/auth.service";
 import React from "react";
 
 const UserPage = () => {
+  const { _id, role } = getUserInfo() as any;
+
+  const { data, isLoading } = useGetSingleUserQuery(_id);
+
+  const userInfo = data?.data;
   return (
     <div>
-      <h1>This is user page</h1>
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+        ]}
+      />
+      <h1
+        style={{
+          marginBottom: "20px",
+          color: "green",
+        }}
+      >
+        Admin Information
+      </h1>
+
+      <h1>Name: {userInfo?.name}</h1>
+      <h1>Email: {userInfo?.email}</h1>
+      <h1>ContactNo: {userInfo?.contactNo}</h1>
+      <h1>Address: {userInfo?.address}</h1>
     </div>
   );
 };
